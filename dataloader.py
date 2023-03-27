6a93a6488ff006bd3d2eb1b29a95d86acdf5f95e
jangroter <47716241+jangroter@users.noreply.github.com>
2023-03-27 17:31:15 +0200

    Changed the format of the MFLoader
    Changed the  __getitem__ function such that it is coherent with the expected functionality of the function for the dataloader class of pytorch

--- dataloader.py
+++ dataloader.py
@@ -1,8 +1,19 @@
 from torch.utils.data import DataLoader
-import data_utils.MFLoader as mf
+import data_utils.MFLoader_correct as mf
 import numpy as np
+import matplotlib.pyplot as plt
 
 train_data = mf.MFLoader(32,'train')
-sample = train_data.dataset.__getitem__(3)
+image, target = train_data.dataset.__getitem__(1)
 
-print(sample['polygons_list'])
+# ensure that the masks have the correct shape
+print(target['masks'].shape)
+
+# plot one of the masks
+image2 = target['masks'][1,:,:].numpy()
+# check to see if the bounding box coordinates correspond with the mask
+print(target['boxes'][1])
+
+plt.figure()
+plt.imshow(image2)
+plt.show()
