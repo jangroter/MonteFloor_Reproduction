1921e44ff99b97f70041e336ca8688d62a63dfbb
jangroter <47716241+jangroter@users.noreply.github.com>
2023-03-28 16:35:56 +0200

    temp quantative analyser, beetje een rotzooi

--- output_generator.py
+++ output_generator.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import torchvision.models.detection as models
 
 train_data = mf.MFLoader(2,'train')
-image, target = train_data.dataset.__getitem__(1)
+image, target = train_data.dataset.__getitem__(3)
 
 model = models.maskrcnn_resnet50_fpn()
 model.load_state_dict(torch.load('weights_maskrcnn'))
@@ -18,12 +18,13 @@ output = model(imaget)
 
 print(output)
 
-plt.figure()
+
 plt.figure()
 plt.subplot(121)
-plt.imshow(image)
+plt.imshow(image*-1)
 plt.subplot(122)
-plt.imshow(output[0]['masks'][0].view(256,256).detach().numpy())
+image2 = output[0]['masks'][0].view(256,256).detach().numpy() + output[0]['masks'][1].view(256,256).detach().numpy() + output[0]['masks'][3].view(256,256).detach().numpy() + output[0]['masks'][4].view(256,256).detach().numpy() + output[0]['masks'][5].view(256,256).detach().numpy()
+plt.imshow(image2)
 
 # plt.imshow(sample["input_map"][:,:,1])
 
